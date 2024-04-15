80ae15fce86be9bba89fbc6cb897861ec8b1c8a9
svereschako <33933400+svereschako@users.noreply.github.com>
2024-04-16 00:54:21 +0200

    fix: linter errors

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -144,7 +144,12 @@ async function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(promises) {  
+function queuPromises(promises) {
+  return promises.reduce(async (previousPromise, nextPromise) => {
+    const result = await previousPromise;
+    const nextResult = await nextPromise;
+    return result + nextResult;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
